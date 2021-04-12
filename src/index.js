b192fca2ecbd772f57d559a748e99e023f2f6bda
ivan_filipchyk <ivanfiliptschik00@gmail.com>
2021-04-12 09:40:13 +0300

    rebuild class

--- src/index.js
+++ src/index.js
@@ -5,35 +5,55 @@ class CountdownTimer {
         this.selector = selector;
         this.targetDate = targetDate;
         this.refs = {
-            timerId: document.querySelector('.timer-1'),
-            days: document.querySelector('[data-value="days"]'),
-            hours: document.querySelector('[ data-value="hours"]'),
-            mins: document.querySelector('[data-value="mins"]'),
-            secs: document.querySelector('[data-value="secs"]') 
+          timerId: document.querySelector('.timer-1'),
+          days: document.querySelector('[data-value="days"]'),
+          hours: document.querySelector('[ data-value="hours"]'),
+          mins: document.querySelector('[data-value="mins"]'),
+          secs: document.querySelector('[data-value="secs"]')
+        };
+        this.time = null;
+        this.date = {
+          days: null,
+          hours: null,
+          mins: null,
+          secs: null,
         }
     }
 
+    start() {
+      this.timer();
+  }
+  
     timer() {
         setInterval(() => {
-        const currentDate = Date.now()
-        const time = this.targetDate - currentDate;
+          const currentDate = Date.now()
+          this.time = this.targetDate - currentDate;
 
-        const days = Math.floor(time / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-        const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-        this.refs.days.textContent = days;
-        this.refs.hours.textContent = hours;
-        this.refs.mins.textContent = mins;
-        this.refs.secs.textContent = secs;           
+          this.getDateToTimer()
+          this.getValueToInterval()
         }, 1000);
-    }
+  }
+
+    getDateToTimer() {
+        this.date.days = Math.floor(this.time / (1000 * 60 * 60 * 24));
+        this.date.hours = Math.floor((this.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        this.date.mins = Math.floor((this.time % (1000 * 60 * 60)) / (1000 * 60));
+        this.date.secs = Math.floor((this.time % (1000 * 60)) / 1000);
+
+  }
+  
+    getValueToInterval() {
+        this.refs.days.textContent = this.date.days;
+        this.refs.hours.textContent = this.date.hours;
+        this.refs.mins.textContent = this.date.mins;
+        this.refs.secs.textContent = this.date.secs; 
+  }
 }
+
 
 const newTimer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jul 17, 2021'),
 });
 
-newTimer.timer();
+newTimer.start();
