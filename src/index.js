2052ce00f9fefa5acb4dd59578147d117b9d8920
ivan_filipchyk <70341552+f957-cua@users.noreply.github.com>
2021-08-08 22:21:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ class CountdownTimer {
 
 const newTimer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2021'),
+  targetDate: new Date('Jul 17, 2022'),
 });
 
 newTimer.start();
